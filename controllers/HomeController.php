055ec0903da85ba29cfb705d009f797cac0501f8
Norther85 <43664405+Norther85@users.noreply.github.com>
2024-12-20 04:49:12 +0100

    Create HomeController.php

---
+++ controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+class HomeController
+{
+    public function index($router)
+    {
+        return $router->view('index');
+    }
+
+    public function about($router)
+    {
+        return $router->view('about');
+    }
+
+    public function faq($router)
+    {
+        return $router->view('faq');
+    }
+
+    public function contact($router)
+    {
+        return $router->view('contact');
+    }
+}
